77ca4dfab686e57348028a86370e06de0d475267
Hugo Mesquita <hugomesquitaweb@gmail.com>
2024-12-22 03:52:31 +0000

    init project

---
+++ enlight_lawIn-main/application/controllers/Main.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	function __construct() 
+    {
+        parent::__construct();
+        $this->load->helper(array('form', 'url', 'html')); 
+        $this->load->library(array('form_validation', 'session'));
+    }
+
+	 
+	public function index()
+	{
+
+        $data['livre'] = 'Conteúdo da variável livre';
+
+		$this->template->load('layoutWithoutFooter', 'index', $data);
+	}
+}
